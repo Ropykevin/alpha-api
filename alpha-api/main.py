3ebc748365d9e8d3dc3da61fd591aa84a65e3a25
Ropy Kevin <ropykevin@gmail.com>
2024-02-27 09:23:08 -0900

    update

--- alpha-api/main.py
+++ alpha-api/main.py
@@ -3,9 +3,11 @@ import sentry_sdk
 from flask import flash, jsonify, request
 from sentry_sdk import capture_exception
 from flask_sqlalchemy import SQLAlchemy
-from dbs import Product, app, db,Sales
+from dbs import Product, app, db, Sales
 from flask_cors import CORS
-
+import requests
+from sqlalchemy import func
+from datetime import datetime, date
 
 sentry_sdk.init(
     dsn="https://ae1085d73eeb378b00f5f8aacb4811e7@o4506695501611008.ingest.sentry.io/4506695661584384",
@@ -22,6 +24,7 @@ sentry_sdk.init(
 
 # store & store products
 CORS(app)
+
 
 @app.route("/products", methods=["POST", "GET"])
 def prods():
@@ -79,20 +82,22 @@ def get_product(product_id):
         # capture_exception(e)
         return jsonify({"error": "Internal Server Error"}), 500
 
-@app.route('/sales',methods=['GET','POST'])
+
+@app.route('/sales', methods=['GET', 'POST'])
 def sales():
     if request.method == 'GET':
         try:
-            sales=Sales.query.all()
-            s_dict=[]
+            sales = Sales.query.all()
+            s_dict = []
             for sale in sales:
-                s_dict.append({"id": sale.id, "pid": sale.pid, "quantity": sale.quantity,"created_at": sale.created_at})
+                s_dict.append({"id": sale.id, "pid": sale.pid,
+                              "quantity": sale.quantity, "created_at": sale.created_at})
             return jsonify(s_dict)
         except Exception as e:
             print(e)
             # capture_exception(e)
             return jsonify({})
-        
+
     elif request.method == 'POST':
         if request.is_json:
             try:
@@ -114,6 +119,46 @@ def sales():
         return jsonify({"error": "Method not allowed."}), 400
 
 
+@app.route('/dashboard', methods=["GET"])
+def dashboard():
+    apikey = "2Z5BUFU7HVV3C5ZS"
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=' + apikey
+    response = requests.get(url)
+    data = response.json()
+    exchange_rate = float(
+        data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+
+    today = date.today()
+    start_of_day = datetime.combine(today, datetime.min.time())
+    end_of_day = datetime.combine(today, datetime.max.time())
+
+    # Query to get sales per day
+    sales_per_day = db.session.query(
+        func.date(Sales.created_at).label('date'),# extracts date from created at
+        func.count('*').label('total_sales')# calculate the total number of sales per day
+    ).filter(
+        Sales.created_at >= start_of_day,
+        Sales.created_at <= end_of_day
+    ).group_by(
+        func.date(Sales.created_at)
+    ).all()
+
+    #  to JSON format
+    sales_data = [{'date': str(day), 'total_sales': sales}
+                  for day, sales in sales_per_day]
+    #  sales per product
+    sales_per_product = db.session.query(
+        Sales.pid,
+        func.sum(Sales.quantity).label('total_sales')
+    ).group_by(
+        Sales.pid
+    ).all()
+
+    # to JSON format
+    salesproduct_data = [{'product_id': pid, 'total_sales': total_sales}
+                              for pid, total_sales in sales_per_product]
+
+    return jsonify({'exchange_rate': exchange_rate, 'sales_data': sales_data, 'salesproduct_data': salesproduct_data})
 
 
 
